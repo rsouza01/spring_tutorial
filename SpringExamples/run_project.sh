2a3b375839450495f128df886f54619aefc2342b
Rodrigo Souza <rsouza01@gmail.com>
2017-12-28 22:31:37 -0200

    First commit!

---
+++ SpringExamples/run_project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+mvn exec:java -Dexec.mainClass=com.rsouza01.common.App
---
+++ create_project.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mvn archetype:generate -DgroupId=com.rsouza01.common -DartifactId=SpringExamples 	-DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
