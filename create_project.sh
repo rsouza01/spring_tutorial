47a46833c4955ed2973d9a10a78ccd985ca23821
Rodrigo Souza <rsouza01@gmail.com>
2017-12-28 23:24:00 -0200

    Impressor example

--- create_project.sh
+++ create_project.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn archetype:generate -DgroupId=com.rsouza01.common -DartifactId=SpringExamples 	-DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+mvn archetype:generate -DgroupId=com.rsouza01.common -DartifactId=Impressor 	-DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
